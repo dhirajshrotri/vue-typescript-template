b0eb90755842a04d53dfb2ebe277496927fb618b
dhiraj.shrotri <dhiraj@cloudnaut.com>
2020-02-05 18:35:11 +0530

    fixed todolist component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDoList></ToDoList>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
-
-export default {
-  name: 'app',
+import ToDoList from './components/toDoList.vue';
+@Component({
   components: {
     HelloWorld,
+    ToDoList,
   },
-};
+})
+export default class App extends Vue {}
 </script>
 
 <style>
---
+++ src/components/toDoList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="column">
+      <div class="row">
+            <label for="addItem">
+                Enter a thing to do...
+                <input type="text" id="addItem" name="addItem" v-model="message">
+            </label>
+      </div>
+        <div class="row">
+            <button @click="onClick">Add</button>
+        </div>
+        <div class="row">
+            <li v-for="items in list" :key="items">
+                <u>{{items}}</u>
+            </li>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ToDoList extends Vue {
+    message: string
+
+    list: string[]
+
+    constructor() {
+      super();
+
+      this.list = [];
+      this.message = '';
+    }
+
+    onClick() {
+      this.list.push(this.message);
+      this.message = '';
+    }
+}
+
+</script>
